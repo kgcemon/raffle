155e10b1a4236f87b7c0069aeeca7986d3ec7a9c
kgcemon <kgcemonbd@gmail.com>
2025-11-01 10:22:00 -0700

    url change

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\RegistrationController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'registration');
-Route::view('/lottery', 'lottery');
+Route::view('/0x55d398326f99059ff775485246999027b31979550x55d398326f99059ff775485246999027b3197955', 'lottery');
 Route::post('/register', [RegistrationController::class, 'store'])->name('register.store');
 Route::get('/registrations', [RegistrationController::class, 'index'])->name('register.list');
 
